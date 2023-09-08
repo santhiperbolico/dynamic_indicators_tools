144396cc58330d9e7cb5e04fd4fc1464d2efdc54
Santiago Arranz <santiago.arranz.sanz@gmail.com>
2023-09-08 16:36:01 +0200

    Ejemplos de péndulo

---
+++ examples/pendulum_simple/main_process.py
@@ -0,0 +1,13 @@
+import os
+
+from dynamic_indicators_tools.main_dynamic_indicators_process import cmm_process_dynamic_indicators
+
+PATH = "examples/pendulum_simple/config_files/"
+
+CONFIG_FILES = [
+    os.path.join(PATH, "config_main_pendulum_system_t_10.json"),
+    os.path.join(PATH, "config_main_pendulum_system_t_30.json"),
+]
+
+if __name__ == "__main__":
+    cmm_process_dynamic_indicators(CONFIG_FILES)
