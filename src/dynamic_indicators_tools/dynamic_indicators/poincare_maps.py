7e8b269c3421a904bc686dc3da7afc9803abbaac
Santiago Arranz <santiago.arranz.sanz@gmail.com>
2023-12-18 00:56:58 +0100

    Implementación del las funciones PoincareSectionGrid
    Se han creado las funciones que calculan los puntos de la sección de poincaré como métodos de una clase del tipo PoincaréSectionGrid. Se ha implementado los tests.

--- src/dynamic_indicators_tools/dynamic_indicators/poincare_maps.py
+++ src/dynamic_indicators_tools/dynamic_indicators/poincare_maps.py
@@ -1,89 +1,313 @@
-from typing import Any, Callable, Dict, Sequence
+from abc import ABC, abstractmethod
+from typing import Any, Callable, Dict, Sequence, Tuple, Union
 
 import numpy as np
+from attr import attrs
 from scipy.optimize import root_scalar
 
 from dynamic_indicators_tools.differentials_systems.diff_system import DiffSystem, DiffVariable
 
+PoincareMapFunction = Callable[[Union[float, np.ndarray]], Union[float, np.ndarray]]
 
-def poincare_section_generator(diff_var: DiffVariable, poincare_map):
-    def init(t):
+
+def poincare_section_generator(
+    diff_var: DiffVariable, poincare_map: PoincareMapFunction
+) -> PoincareMapFunction:
+    """
+    Función que genera la función de la sección de poincaré compuesta de la interpolación
+    de la solución de un sistema diferencial dado.
+
+    Parameters
+    ----------
+    diff_var: DiffVariable
+        Variable de un sistema diferencial que tiene almacenada la interpolación
+        de la solución de dicho sistema diferencial.
+    poincare_map: PoincareMapFunction
+        Función que representa la sección de Poincaré, siendo está los puntos
+        que anulan a la función.
+
+    Returns
+    -------
+    init: PoincareMapFunction
+        Función de poincaré compuesta con la solución x de un sistema diferencial
+        De esta manera para un tiempo t devuelve el valor de x(t) de la función
+        de Poincaré original.
+
+    """
+
+    def init(t: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
         return poincare_map(diff_var.solution(t))
 
     return init
 
 
-def poincare_section_grid(
-    diff_system: DiffSystem,
-    poincare_map: Callable[[np.ndarray], float],
-    solver_method: str,
-    t_span: Sequence[float],
-    x0: np.ndarray,
-    n_points: int = 100,
-    args: Sequence[Any] = None,
-    params_solver: Dict[str, Any] = None,
-    params_root: Dict[str, Any] = None,
-) -> np.ndarray:
-    if params_root is None:
-        params_root = {"method": "bisect"}
+class PoincareSectionGrid(ABC):
+    """
+    Interfaz con la estructura de los métodos determinados
+    a calcular las secciones de Poincaré de un sistema dado.
+    """
 
-    _, _ = diff_system.solve_function(solver_method, t_span, x0, args, params_solver)
-    poincare_section = poincare_section_generator(diff_system.variable, poincare_map)
-    t_values = np.linspace(t_span[0], t_span[1], n_points)
-    t_roots = []
-    for i in range(n_points - 1):
-        root = None
-        try:
-            params_root.update({"bracket": [t_values[i], t_values[i + 1]]})
-            root_result = root_scalar(poincare_section, **params_root)
-            converged = root_result.converged
-            root = root_result.root
-        except ValueError:
-            converged = False
-        if converged:
-            t_roots.append(root)
-    t_roots = np.array(t_roots)
-    return t_roots
+    @staticmethod
+    @abstractmethod
+    def get_poincare_points(
+        diff_system: DiffSystem,
+        poincare_map: PoincareMapFunction,
+        solver_method: str,
+        t_span: Sequence[float],
+        x0: np.ndarray,
+        n_points: int = 100,
+        args: Sequence[Any] = None,
+        params_solver: Dict[str, Any] = None,
+        params_root: Dict[str, Any] = None,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Método que calcula los puntos de tiempo y del espacio para la sección
+        de poincaré indicada en el sistema indicado.
+
+        Parameters
+        ----------
+        diff_system: DiffSystem
+            Sistema diferencial del cual se quiere extraer los puntos de la sección de poinacaré.
+        poincare_map: Callable[[np.ndarray], float],
+            Función de la sección de Poincaré. La sección sería el conjunto de todos aquellos
+            puntos que anulan a la función.
+        solver_method: str
+            Nombre del método para resolver DiffSystem. Ver DiffSystem.solve_function.
+        t_span: Sequence[float]
+            Intervalo de tiempo del cual se quiere extraer los puntos de Poincaré.
+            Ver DiffSystem.solve_function
+        x0: np.ndarray
+            Condición inicial del sistema diferencial. Ver DiffSystem.solve_function
+        n_points: int, default 100
+            Número de puntos en el que se va a dividir el intervalo de tiempo para buscar
+            los puntos de la solución en la sección de Poincaré.
+        args: Sequence[Any], default None
+            Argumentos asociados a la función del sistema diferencial.
+            Ver DiffSystem.solve_function
+        params_solver: Dict[str, Any], default None
+            Parámetros del solver usado para resolver el sistema diferencial.
+            Ver DiffSystem.solve_function
+        params_root: Dict[str, Any], default None
+            Parámetros del método para encontrar las raices de la función poincare_map
+
+        Returns
+        -------
+        t_roots: np.ndarray
+            Array con los valores de tiempo asociados a las intersecciones con la
+            sección de Poincaré.
+        values__roots: np.ndarray
+            Valores de la solución en los tiempos de t_roots.
+        """
+        pass
 
 
-def poincare_section_grid_ode(
-    diff_system: DiffSystem,
-    poincare_map: Callable[[np.ndarray], float],
-    solver_method: str,
-    t_span: Sequence[float],
-    x0: np.ndarray,
-    n_points: int = 100,
-    args: Sequence[Any] = None,
-    params_solver: Dict[str, Any] = None,
-    params_root: Dict[str, Any] = None,
-):
-    if params_root is None:
-        params_root = {"method": "bisect"}
+@attrs
+class PoincareSectionInterpolate(PoincareSectionGrid):
+    name = "PoincareSectionInterpolate"
 
-    if params_solver is None:
-        params_solver = {}
+    @staticmethod
+    def get_poincare_points(
+        diff_system: DiffSystem,
+        poincare_map: PoincareMapFunction,
+        solver_method: str,
+        t_span: Sequence[float],
+        x0: np.ndarray,
+        n_points: int = 100,
+        args: Sequence[Any] = None,
+        params_solver: Dict[str, Any] = None,
+        params_root: Dict[str, Any] = None,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Método que calcula los puntos de tiempo y del espacio para la sección
+        de poincaré indicada en el sistema indicado. Para ello resuelve el sistema
+        diferencial e interpola la solución. Después, usando la función root_scalar
+        de scipy.optimize en n_points-1 sub-intervalos de tiempo encuentra los
+        valores de las raices de la función de la sección de Poincaré compuesta
+        de la interpolación de la solución.
 
-    params_general_solver = params_solver.copy()
-    if n_points:
-        t_values = np.linspace(t_span[0], t_span[1], n_points)
-        params_general_solver.update({"t_eval": t_values})
-    t_values, x0_array = diff_system.solve_function(
-        solver_method, t_span, x0, args, params_general_solver
-    )
-    t_roots = []
-    for i in range(t_values.size - 1):
-        _, _ = diff_system.solve_function(
-            solver_method, [t_values[i], t_values[i + 1]], x0_array[:, i], args, params_solver
-        )
+        Parameters
+        ----------
+        diff_system: DiffSystem
+            Sistema diferencial del cual se quiere extraer los puntos de la sección de poinacaré.
+        poincare_map: PoincareMapFunction,
+            Función de la sección de Poincaré. La sección sería el conjunto de todos aquellos
+            puntos que anulan a la función.
+        solver_method: str
+            Nombre del método para resolver DiffSystem. Ver DiffSystem.solve_function.
+        t_span: Sequence[float]
+            Intervalo de tiempo del cual se quiere extraer los puntos de Poincaré.
+            Ver DiffSystem.solve_function
+        x0: np.ndarray
+            Condición inicial del sistema diferencial. Ver DiffSystem.solve_function
+        n_points: int, default 100
+            Número de puntos en el que se va a dividir el intervalo de tiempo para buscar
+            los puntos de la solución en la sección de Poincaré.
+        args: Sequence[Any], default None
+            Argumentos asociados a la función del sistema diferencial.
+            Ver DiffSystem.solve_function
+        params_solver: Dict[str, Any], default None
+            Parámetros del solver usado para resolver el sistema diferencial.
+            Ver DiffSystem.solve_function
+        params_root: Dict[str, Any], default None
+            Parámetros del método para encontrar las raices de la función poincare_map.
+            Ver scipy.optimize.root_scalar
+
+        Returns
+        -------
+        t_roots: np.ndarray
+            Array con los valores de tiempo asociados a las intersecciones con la
+            sección de Poincaré.
+        values__roots: np.ndarray
+            Valores de la solución en los tiempos de t_roots.
+        """
+        if params_root is None:
+            params_root = {"method": "bisect"}
+
+        _, _ = diff_system.solve_function(solver_method, t_span, x0, args, params_solver)
         poincare_section = poincare_section_generator(diff_system.variable, poincare_map)
-        root = None
-        params_root.update({"bracket": [t_values[i], t_values[i + 1]]})
-        try:
-            root_result = root_scalar(poincare_section, **params_root)
-            converged = root_result.converged
-            root = root_result.root
-        except ValueError:
-            converged = False
-        if converged:
-            t_roots.append(root)
-    return np.array(t_roots)
+        t_values = np.linspace(t_span[0], t_span[1], n_points)
+        t_roots = []
+        for i in range(n_points - 1):
+            root = None
+            try:
+                params_root.update({"bracket": [t_values[i], t_values[i + 1]]})
+                root_result = root_scalar(poincare_section, **params_root)
+                converged = root_result.converged
+                root = root_result.root
+            except ValueError:
+                converged = False
+            if converged:
+                t_roots.append(root)
+        t_roots = np.array(t_roots)
+        values_roots = diff_system.variable.solution(t_roots).T
+        return t_roots, values_roots
+
+
+@attrs
+class PoincareSectionOdeTimeRange(PoincareSectionGrid):
+    name = "PoincareSectionOdeTimeRange"
+
+    @staticmethod
+    def get_poincare_points(
+        diff_system: DiffSystem,
+        poincare_map: PoincareMapFunction,
+        solver_method: str,
+        t_span: Sequence[float],
+        x0: np.ndarray,
+        n_points: int = 100,
+        args: Sequence[Any] = None,
+        params_solver: Dict[str, Any] = None,
+        params_root: Dict[str, Any] = None,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Método que calcula los puntos de tiempo y del espacio para la sección
+        de poincaré indicada en el sistema indicado. Para ello, resuelve el sistema diferencial
+        sacando los valores para n_points de tiempo. Después, usando los valores para esos tiempos
+        vuelve a resolver el sistema para cada uno de los n_points-1 subintervalos de tiempo,
+        resolviendo el sistema, interpolando la solución y encontrando la raiz de la función
+        de la sección de Poincaré compuesta de la interpolación de la solución del sistema
+        diferencial.
+
+        Parameters
+        ----------
+        diff_system: DiffSystem
+            Sistema diferencial del cual se quiere extraer los puntos de la sección de poinacaré.
+        poincare_map: PoincareMapFunction,
+            Función de la sección de Poincaré. La sección sería el conjunto de todos aquellos
+            puntos que anulan a la función.
+        solver_method: str
+            Nombre del método para resolver DiffSystem. Ver DiffSystem.solve_function.
+        t_span: Sequence[float]
+            Intervalo de tiempo del cual se quiere extraer los puntos de Poincaré.
+            Ver DiffSystem.solve_function
+        x0: np.ndarray
+            Condición inicial del sistema diferencial. Ver DiffSystem.solve_function
+        n_points: int, default 100
+            Número de puntos en el que se va a dividir el intervalo de tiempo para buscar
+            los puntos de la solución en la sección de Poincaré.
+        args: Sequence[Any], default None
+            Argumentos asociados a la función del sistema diferencial.
+            Ver DiffSystem.solve_function
+        params_solver: Dict[str, Any], default None
+            Parámetros del solver usado para resolver el sistema diferencial.
+            Ver DiffSystem.solve_function
+        params_root: Dict[str, Any], default None
+            Parámetros del método para encontrar las raices de la función poincare_map.
+            Ver scipy.optimize.root_scalar
+
+        Returns
+        -------
+        t_roots: np.ndarray
+            Array con los valores de tiempo asociados a las intersecciones con la
+            sección de Poincaré.
+        values__roots: np.ndarray
+            Valores de la solución en los tiempos de t_roots.
+        """
+        if params_root is None:
+            params_root = {"method": "bisect"}
+
+        if params_solver is None:
+            params_solver = {}
+
+        params_general_solver = params_solver.copy()
+        if n_points:
+            t_values = np.linspace(t_span[0], t_span[1], n_points)
+            params_general_solver.update({"t_eval": t_values})
+        t_values, x0_array = diff_system.solve_function(
+            solver_method, t_span, x0, args, params_general_solver
+        )
+        t_roots = []
+        values_roots = []
+        for i in range(t_values.size - 1):
+            t_span_iteration = [float(t_values[i]), float(t_values[i + 1])]
+            x0_iteration = x0_array[i, :]
+            _, _ = diff_system.solve_function(
+                solver_method=solver_method,
+                t_span=t_span_iteration,
+                x0=x0_iteration,
+                args=args,
+                params_solver=params_solver,
+            )
+            poincare_section = poincare_section_generator(diff_system.variable, poincare_map)
+            params_root.update({"bracket": [t_values[i], t_values[i + 1]]})
+            try:
+                root_result = root_scalar(poincare_section, **params_root)
+                converged = root_result.converged
+                root = root_result.root
+            except ValueError:
+                root = None
+                converged = False
+            if converged:
+                values_roots.append(list(diff_system.variable.solution(root)))
+                t_roots.append(root)
+        t_roots = np.array(t_roots)
+        values_roots = np.array(values_roots)
+        return t_roots, values_roots
+
+
+def get_poincare_grid_method(method: str) -> PoincareSectionGrid.get_poincare_points:
+    """
+    Función que devuelve el método  get_poincare_points de la clase
+    PoincareSectionGrid asociada al método.
+
+    Parameters
+    ----------
+    method: str
+        Nombre de la clase del tipo PoincareSectionGrid que se va a utilizar.
+
+    Returns
+    -------
+    get_poincare_points: PoincarePointsFunction
+        Función get_poincare_points de la clase asociada.
+    """
+    dic_methods = {
+        PoincareSectionInterpolate.name: PoincareSectionInterpolate.get_poincare_points,
+        PoincareSectionOdeTimeRange.name: PoincareSectionOdeTimeRange.get_poincare_points,
+    }
+    try:
+        return dic_methods[method]
+    except KeyError:
+        raise ValueError(
+            f"El método de Poincaré {method} no está implementado."
+            f"Usa alguno de {list(dic_methods.keys())}"
+        )
--- src/tests/test_poincare_map.py
+++ src/tests/test_poincare_map.py
@@ -2,7 +2,10 @@ import numpy as np
 import pytest
 
 from dynamic_indicators_tools.differentials_systems.diff_system import DiffSystem, DiffVariable
-from dynamic_indicators_tools.dynamic_indicators.poincare_maps import poincare_section_grid
+from dynamic_indicators_tools.dynamic_indicators.poincare_maps import (
+    PoincareMapFunction,
+    get_poincare_grid_method,
+)
 
 
 @pytest.fixture
@@ -16,7 +19,7 @@ def lorentz_system():
         x = variable.values[:, 1]
         y = variable.values[:, 2]
         z = variable.values[:, 3]
-        return sigma * (y - x), x * (rho - z) - y, x * y - beta * z
+        return np.concatenate((sigma * (y - x), x * (rho - z) - y, x * y - beta * z))
 
     return DiffSystem(var, lorentz_function)
 
@@ -29,12 +32,37 @@ def poincare_map():
     return function
 
 
-def test_poincare_section_grid(lorentz_system, poincare_map):
+@pytest.mark.parametrize("method", ["PoincareSectionInterpolate", "PoincareSectionOdeTimeRange"])
+@pytest.mark.parametrize(
+    "t_max, expected_t_roots, expected_values",
+    [
+        (10, np.array([0.51298646]), np.array([[5.21529486e-11, -8.82950611e00, 3.13821647e01]])),
+        (
+            15,
+            np.array([0.51298646, 14.17828829, 14.95354952]),
+            np.array(
+                [
+                    [-3.21422888e-11, -8.82950611e00, 3.13821647e01],
+                    [-7.69295738e-12, 1.66700449e00, 2.17284880e01],
+                    [1.27770017e-11, -1.95144391e00, 2.24303988e01],
+                ]
+            ),
+        ),
+    ],
+)
+def test_poincare_section_grid(
+    method: str,
+    t_max: float,
+    expected_t_roots: np.ndarray,
+    expected_values: np.ndarray,
+    lorentz_system: DiffSystem,
+    poincare_map: PoincareMapFunction,
+):
     x0 = np.array([1.0, 1.0, 1.0])
-    T = 40
-    t_span = [0, T]
-    n_points = T * 100
-    result = poincare_section_grid(
+    t_span = [0, t_max]
+    n_points = t_max * 100
+    poincare_method = get_poincare_grid_method(method)
+    result_t_roots, result_values = poincare_method(
         diff_system=lorentz_system,
         poincare_map=poincare_map,
         solver_method="solve_ivp",
@@ -43,25 +71,10 @@ def test_poincare_section_grid(lorentz_system, poincare_map):
         n_points=n_points,
     )
 
-    expected = np.array(
-        [
-            0.51298646,
-            14.17828829,
-            14.95354847,
-            15.74609354,
-            16.51259702,
-            18.0835094,
-            20.36845722,
-            22.63899511,
-            24.18033758,
-            24.94841309,
-            27.4313825,
-            32.97882687,
-            34.50835275,
-            35.27987797,
-            36.09752353,
-            36.8684677,
-            39.12347803,
-        ]
-    )
-    assert result == pytest.approx(expected, 1e-3)
+    assert result_t_roots == pytest.approx(expected_t_roots, 1e-4)
+    assert (np.abs(result_values - expected_values) < 1e-3).all()
+
+
+def test_get_poincare_grid_method_error():
+    with pytest.raises(ValueError):
+        _ = get_poincare_grid_method("method_error")
