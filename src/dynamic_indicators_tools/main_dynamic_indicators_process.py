44db11f9ac6c6c6bb7f27f7af601f86f55fdb26f
Santiago Arranz <santiago.arranz.sanz@gmail.com>
2024-04-07 19:08:13 +0200

    Creación de direcorio logs y file

--- src/dynamic_indicators_tools/main_dynamic_indicators_process.py
+++ src/dynamic_indicators_tools/main_dynamic_indicators_process.py
@@ -1,12 +1,19 @@
 import json
 import logging
+import os
+from datetime import datetime
 from typing import List, Union
 
 from dynamic_indicators_tools.dynamic_indicators.dynamic_indicators_utils import main_process_di
 
 
 def multi_process_dynamic_indicators(config_json_path: Union[str, List[str]]) -> None:
-    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] [%(asctime)s] %(message)s")
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    path_log = os.path.join("logs", f"log_{datetime.today().strftime('%Y%m%d_%H%M')}.txt")
+    logging.basicConfig(
+        filename=path_log, level=logging.INFO, format="[%(levelname)s] [%(asctime)s] %(message)s"
+    )
 
     if isinstance(config_json_path, str):
         config_json_path = [config_json_path]
