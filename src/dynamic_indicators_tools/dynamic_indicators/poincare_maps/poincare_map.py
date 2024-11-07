1ba8eb1538954995edc5d655b5450a1ed9f271e9
Santiago Arranz <santiago.arranz.sanz@gmail.com>
2024-11-07 17:50:11 +0100

    [TFMS-59] Se ha creado la función que devuelve el jacobiando de poincare_map_function

--- src/dynamic_indicators_tools/dynamic_indicators/poincare_maps/poincare_map.py
+++ src/dynamic_indicators_tools/dynamic_indicators/poincare_maps/poincare_map.py
@@ -2,13 +2,14 @@ from typing import Callable, Union
 
 import numpy as np
 
-from dynamic_indicators_tools.differentials_systems.diff_system import FlowMap
+from dynamic_indicators_tools.differentials_systems.diff_system import DiffVariable, FlowMap
 from dynamic_indicators_tools.dynamic_indicators.poincare_maps.poincare_utils import (
     PoincareMapFunction,
     roots_function,
 )
 
 PoincareMap = Callable[[np.ndarray], float]
+GradientPoincareMap = Callable[[np.ndarray], np.ndarray]
 
 
 def poincare_map_restriction_generator(
@@ -68,14 +69,41 @@ def poincare_map_restriction_generator(
     return poincare_map_restriction
 
 
-def poincare_map_function(
+def get_t_root_n_iter(
     poincare_map: PoincareMap,
     flow: FlowMap,
     x0: np.ndarray,
     tau_max: Union[float, int],
     n_iter: int = 1,
     n_points: int = 10,
-):
+) -> Union[float, int]:
+    """
+    Función que calcula el valor t_i donde la órbita del flujo de condición inicial x0 ha pasado
+    n_iter veces por la sección de poincaré.
+
+    Parameters
+    ----------
+    poincare_map: PoincareMap
+        Función R^n->R que genera la sección de Poincaré.
+    flow: FlowMap
+        Flujo del sistema diferencial.
+    x0: np.ndarray
+        Condición inicial del sistema para fow.t0.
+    tau_max: Union[float, int]
+        Incremento del tiempo donde se van a buscar los valores de la función.
+        El sistema diferencial se resolverá en un intervalo [flow.t0, flow.t0 + tau_max].
+    n_iter: int, default 1
+        Número de pasos por la sección de Poincaré.
+    n_points: int, default 10
+        Número de puntos para los que se van a buscar soluciones
+        en el intervalo [flow.t0, flow.t0 + tau_max]
+
+    Returns
+    -------
+    t_i: Union[float, int]
+        Valor del tiempo dónde la órbita del flujo para x0 ha pasado n_iter veces por la sección
+        de poincaré.
+    """
     poincare_map_restriction = poincare_map_restriction_generator(poincare_map, flow, x0)
     t_roots = roots_function(poincare_map_restriction, t_span=[0, tau_max], n_points=n_points)
     t_roots = t_roots + flow.t0
@@ -85,4 +113,123 @@ def poincare_map_function(
             "pruebe a ampliar el t_span."
         )
     t_i = t_roots[n_iter - 1]
+    return t_i
+
+
+def poincare_map_function(
+    poincare_map: PoincareMap,
+    flow: FlowMap,
+    x0: np.ndarray,
+    tau_max: Union[float, int],
+    n_iter: int = 1,
+    n_points: int = 10,
+) -> np.ndarray:
+    """
+    Función que devuelve el valor del flujo para las n_iter pasos de la órbita del flujo
+    en la sección de poincaré marcada por poincare_map.
+
+    Parameters
+    ----------
+    poincare_map: PoincareMap
+        Función R^n->R que genera la sección de Poincaré.
+    flow: FlowMap
+        Flujo del sistema diferencial.
+    x0: np.ndarray
+        Condición inicial del sistema para fow.t0.
+    tau_max: Union[float, int]
+        Incremento del tiempo donde se van a buscar los valores de la función.
+        El sistema diferencial se resolverá en un intervalo [flow.t0, flow.t0 + tau_max].
+    n_iter: int, default 1
+        Número de pasos por la sección de Poincaré.
+    n_points: int, default 10
+        Número de puntos para los que se van a buscar soluciones
+        en el intervalo [flow.t0, flow.t0 + tau_max]
+
+    Returns
+    -------
+    xi: np.ndarray
+        Punto en la sección de poincaré igual al valor de la órbita del flujo para x0, después
+        de haber pasado n_iter veces por la sección.
+    """
+    t_i = get_t_root_n_iter(poincare_map, flow, x0, tau_max, n_iter, n_points)
     return flow(t_i, x0)
+
+
+def get_dimension_from_variational_equations(x0: np.ndarray) -> int:
+    """
+    Función que extrae la dimensión de las variables de un sistema diferencial
+    de una condición inicial correspondiente de su sistema diferencial de ecuaciones
+    variacionales.
+
+    Parameters
+    ----------
+    x0: np.ndarray
+        Condición inicial de un sistema diferencial de ecuaciones variacionales.
+
+    Returns
+    -------
+    n: int
+        Dimensión del problema original, eliminando las ecuaciones variacionales.
+    """
+    m = x0.size
+    n = (np.sqrt(1 + 4 * m) - 1) / 2
+    if n % 1 > 0:
+        raise ValueError(
+            "La dimensión de x0 no se corresponde a la de una condición"
+            " inicial de un sistema de ecuaciones variacionales."
+        )
+    return int(n)
+
+
+def diff_poincare_map_function(
+    poincare_map: PoincareMap,
+    gradient_poincare_map: GradientPoincareMap,
+    flow: FlowMap,
+    x0: np.ndarray,
+    tau_max: Union[float, int],
+    n_iter: int = 1,
+    n_points: int = 10,
+) -> np.ndarray:
+    """
+    Función que devuelve el valor del jacobiano de la función poincare_map_function, la cual
+    devuelve el valor del flujo para las n_iter pasos de la órbita del flujo en la sección de
+    poincaré marcada por poincare_map.
+
+    Parameters
+    ----------
+    poincare_map: PoincareMap
+        Función R^n->R que genera la sección de Poincaré.
+    gradient_poincare_map: GradientPoincareMap
+        Función R^n->R^n del gradiente de poincare_map
+    flow: FlowMap
+        Flujo del sistema diferencial.
+    x0: np.ndarray
+        Condición inicial del sistema para fow.t0.
+    tau_max: Union[float, int]
+        Incremento del tiempo donde se van a buscar los valores de la función.
+        El sistema diferencial se resolverá en un intervalo [flow.t0, flow.t0 + tau_max].
+    n_iter: int, default 1
+        Número de pasos por la sección de Poincaré.
+    n_points: int, default 10
+        Número de puntos para los que se van a buscar soluciones
+        en el intervalo [flow.t0, flow.t0 + tau_max]
+
+    Returns
+    -------
+    xi: np.ndarray
+        Punto en la sección de poincaré igual al valor de la órbita del flujo para x0, después
+        de haber pasado n_iter veces por la sección.
+    """
+    t_i = get_t_root_n_iter(poincare_map, flow, x0, tau_max, n_iter, n_points)
+    values = flow(t_i, x0).T
+    t_i_array = np.ones((values.shape[1], 1)) * t_i
+    n_var = get_dimension_from_variational_equations(values)
+    xi, jac_flow = values[:n_var], values[n_var:].reshape((n_var, n_var))
+    df_var_xi = DiffVariable("xi", np.concatenate((t_i_array, values.T), axis=1))
+    f_xi = flow.diff_system.function(df_var_xi).reshape(-1, 1)
+
+    projection = np.dot(gradient_poincare_map(xi).T, jac_flow) / np.dot(
+        gradient_poincare_map(xi).T, f_xi[:n_var]
+    )
+    projection = np.dot(f_xi[:n_var], projection)
+    return jac_flow - projection
--- src/tests/test_poincare_map.py
+++ src/tests/test_poincare_map.py
@@ -7,6 +7,7 @@ from dynamic_indicators_tools.differentials_systems.diff_system import (
     FlowMap,
 )
 from dynamic_indicators_tools.dynamic_indicators.poincare_maps.poincare_map import (
+    diff_poincare_map_function,
     poincare_map_function,
     poincare_map_restriction_generator,
 )
@@ -34,9 +35,72 @@ def lorenz_system():
 
 
 @pytest.fixture
+def lorenz_system_variational():
+    rho = 28.0
+    sigma = 10.0
+    beta = 8.0 / 3.0
+    var = DiffVariable("x")
+
+    def lorenz_function(variable: DiffVariable) -> np.ndarray:
+        """
+        Función que construye el sistema de ecuaciones variacionales
+        del sistema diferencial de Lorenz
+            dx = sigma * (y - x)
+            dy = x * (rho - z) - y
+            dz = x * y - beta * z
+        """
+        x = variable.values[:, 1]
+        y = variable.values[:, 2]
+        z = variable.values[:, 3]
+
+        g0 = variable.values[:, 4]
+        g1 = variable.values[:, 5]
+        g2 = variable.values[:, 6]
+        g3 = variable.values[:, 7]
+        g4 = variable.values[:, 8]
+        g5 = variable.values[:, 9]
+        g6 = variable.values[:, 10]
+        g7 = variable.values[:, 11]
+        g8 = variable.values[:, 12]
+
+        fval0 = (sigma * (y - x)).reshape(-1, 1)
+        fval1 = (x * (rho - z) - y).reshape(-1, 1)
+        fval2 = (x * y - beta * z).reshape(-1, 1)
+
+        fval3 = (-sigma * g0 + (rho - z) * g3 + y * g6).reshape(-1, 1)
+        fval4 = (-sigma * g1 + (rho - z) * g4 + y * g7).reshape(-1, 1)
+        fval5 = (-sigma * g2 + (rho - z) * g5 + y * g8).reshape(-1, 1)
+
+        fval6 = (sigma * g0 + (-1) * g3 + x * g6).reshape(-1, 1)
+        fval7 = (sigma * g1 + (-1) * g4 + x * g7).reshape(-1, 1)
+        fval8 = (sigma * g2 + (-1) * g5 + x * g8).reshape(-1, 1)
+
+        fval9 = (-x * g3 - beta * g6).reshape(-1, 1)
+        fval10 = (-x * g4 - beta * g7).reshape(-1, 1)
+        fval11 = (-x * g5 - beta * g8).reshape(-1, 1)
+
+        return np.concatenate(
+            (fval0, fval1, fval2, fval3, fval4, fval5, fval6, fval7, fval8, fval9, fval10, fval11),
+            axis=1,
+        )
+
+    return DiffSystem(var, lorenz_function)
+
+
+@pytest.fixture
 def poincare_map():
     def function(states):
         return states[0]
+
+    return function
+
+
+@pytest.fixture
+def gradient_poincare_map():
+    def function(states):
+        gradient = np.zeros(states.shape)
+        gradient[0] = 1
+        return gradient
 
     return function
 
@@ -188,3 +252,31 @@ def test_poincare_map_function(n_iter, expected, poincare_map, lorenz_system):
     flow.set_params_fun_solver(solver_method="solve_ivp")
     xi = poincare_map_function(poincare_map, flow, x0, 15, n_iter, n_points=750)
     assert xi == pytest.approx(expected, abs=2e-3)
+
+
+@pytest.mark.parametrize(
+    "x0",
+    [
+        (np.array([1.0, 1.0, 1.0])),
+        (np.array([0.8, 0.4, 0.3])),
+        (np.array([0.7, 0.6, 0.8])),
+    ],
+)
+def test_diff_poincare_map_function(
+    x0, poincare_map, gradient_poincare_map, lorenz_system_variational
+):
+    n_iter = 1
+    x0_var = np.concatenate((x0, np.eye(3).reshape(9)))
+    flow = FlowMap(lorenz_system_variational, t0=0)
+    flow.set_params_fun_solver(solver_method="solve_ivp")
+    jac_projection = diff_poincare_map_function(
+        poincare_map=poincare_map,
+        gradient_poincare_map=gradient_poincare_map,
+        flow=flow,
+        x0=x0_var,
+        tau_max=15,
+        n_iter=n_iter,
+        n_points=750,
+    )
+    assert jac_projection.shape == (3, 3)
+    assert jac_projection[0, :] == pytest.approx(np.zeros(3), abs=1e-7)
