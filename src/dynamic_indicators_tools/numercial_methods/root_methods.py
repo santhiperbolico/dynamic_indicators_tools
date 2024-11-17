8358ba53bad23e810f3b01a6527c506286dabf00
Santiago Arranz <santiago.arranz.sanz@gmail.com>
2024-11-17 12:26:47 +0100

    [TFMS-59] Modificación del método de Newton y creación del método de punto fijo

--- src/dynamic_indicators_tools/numercial_methods/root_methods.py
+++ src/dynamic_indicators_tools/numercial_methods/root_methods.py
@@ -1,6 +1,48 @@
-from typing import Callable
+from typing import Callable, Optional
 
 import numpy as np
+from attr import attrs
+
+
+@attrs(auto_attribs=True, repr=False)
+class SummaryRoot:
+    """
+    Clase que recoje el resulta de la búsqueda de raices de una función.
+
+    Attributes
+    ----------
+    method: str
+        Nombre del método utlizado.
+    success: bool
+        Indica si el método ha convergido.
+    root: np.ndarray
+        Raiz encontrada por el método.
+    iter: np.ndarray
+        Iteración donde se ha encontrado la raiz.
+    tol: np.ndarrat
+        Tolerancia obtenida en el método.
+    """
+
+    method: str
+    success: bool
+    root: np.ndarray
+    iter: int
+    tol: float
+
+    def __repr__(self):
+        message_method = f"The method {self.method} hasn't converged.\n"
+
+        if self.success:
+            message_method = f"The method {self.method} has converged.\n"
+
+        message = (
+            f"{message_method}\t SUCCESS: {self.success}\n"
+            f"\t ROOT: {self.root}\n"
+            f"\t ITERATIONS: {self.iter}\n"
+            f"\t TOLERANCE: {self.tol}\n"
+        )
+
+        return message
 
 
 def inverse_product_generator(dimension: int) -> Callable[[np.ndarray, np.ndarray], np.ndarray]:
@@ -40,7 +82,7 @@ def newton_null_point(
     x_0: np.ndarray,
     tol: float = 1e-3,
     max_iteration: int = 1000,
-) -> np.ndarray:
+) -> SummaryRoot:
     """
     Método de Newton para encontrar raices de una función vectorial.
 
@@ -59,7 +101,7 @@ def newton_null_point(
 
     Returns
     -------
-    xi: np.ndarray
+    result: SummaryRoot
         Raiz encontrada.
     """
     inverse_product = inverse_product_generator(x_0.size)
@@ -70,7 +112,68 @@ def newton_null_point(
         fxi = vectorial_function(xi)
         dfxi = jacobian_function(xi)
         xii = xi - inverse_product(fxi, dfxi)
-        atol = np.linalg.norm(xii)
+        atol = np.linalg.norm(xii - xi)
         xi = xii
         iter += 1
-    return xi
+
+    success = True
+    if iter >= max_iteration:
+        success = False
+
+    result = SummaryRoot("Newton", success, xi, iter, atol)
+    return result
+
+
+def newton_fix_point(
+    vectorial_function: Callable[[np.ndarray], np.ndarray],
+    jacobian_function: Callable[[np.ndarray], np.ndarray],
+    x_min: Optional[np.ndarray] = None,
+    x_max: Optional[np.ndarray] = None,
+    x_0: Optional[np.ndarray] = None,
+    tol: float = 1e-3,
+    max_iteration: int = 1000,
+) -> SummaryRoot:
+    """
+    Función que dada una función se encarga de buscar puntos fijos dada una función
+    y su diferencial.
+
+    Parameters
+    ----------
+    vectorial_function: Callable[[np.ndarray], np.ndarray]
+        Función vectorial de la que queremos encontrar sus raices.
+    jacobian_function: Callable[[np.ndarray], np.ndarray]
+        Función que devuelve la jacobianda de l afunción vectorial.
+    x_min: Optional[np.ndarray] = None
+        Valores mínimos para encontrar un punto de partída. Se deben indicar si no se añade
+        un punto de partida.
+    x_max: Optional[np.ndarray] = None
+        Valores mínimos para encontrar un punto de partída. Se deben indicar si no se añade
+        un punto de partida.
+    x_0: Optional[np.ndarray] = None
+        Punto inicial de búsqueda. Se debe indicar si se ha dejado como nulo x_min y x_max. Si
+        no se indica x_0 se busca un valor aleatorio entre x_min y x_max.
+    tol: float, default 1e-3
+        Tolerancia del método. Cuando se alcanza dicha tolerancia el método para.
+    max_iteration: int, default 1000
+        Número de iteraciones máximas del método.
+
+    Returns
+    -------
+    result: SummaryRoot
+        Punto fijo encontrado.
+
+    Raises
+    ------
+    ValueError: Salta si el método no ha encontrado ningún punto fijo.
+    """
+
+    def function(x: np.ndarray) -> np.ndarray:
+        return x - vectorial_function(x)
+
+    def jac_function(x: np.ndarray) -> np.ndarray:
+        return np.eye(x.size) - jacobian_function(x)
+
+    if not isinstance(x_0, (np.ndarray, int, float)):
+        x_0 = np.random.uniform(x_min, x_max)
+    result = newton_null_point(function, jac_function, x_0, tol=tol, max_iteration=max_iteration)
+    return result
--- src/tests/test_numerical_method.py
+++ src/tests/test_numerical_method.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pytest
 
-from dynamic_indicators_tools.numercial_methods.root_methods import newton_null_point
+from dynamic_indicators_tools.numercial_methods.root_methods import (
+    newton_fix_point,
+    newton_null_point,
+)
 
 
 def function1d():
@@ -29,7 +32,24 @@ def function2d():
 
 def jacfunction2d():
     def inner(x: np.ndarray) -> np.ndarray:
-        jac = np.array([[1 - np.exp(x[0]), 0], [0, 1 + np.cos(x[1])]])
+        jac = np.array([[1 - np.exp(x[0]), 0], [0, 1 + np.sin(x[1])]])
+        return jac
+
+    return inner
+
+
+def functionfix():
+    def inner(x: np.ndarray) -> np.ndarray:
+        fx0 = np.exp(x[0]) - 1
+        fx1 = np.cos(x[1])
+        return np.array([fx0, fx1])
+
+    return inner
+
+
+def jacfunctionfix():
+    def inner(x: np.ndarray) -> np.ndarray:
+        jac = np.array([[np.exp(x[0]), 0], [0, -np.sin(x[1])]])
         return jac
 
     return inner
@@ -46,4 +66,23 @@ def test_newton_null_point(function, jac, expected):
     tol = 1e-7
     x0 = np.ones(expected.shape)
     result = newton_null_point(function, jac, x0, tol=tol)
-    assert result == pytest.approx(expected, abs=tol)
+    assert result.success
+    assert result.root == pytest.approx(expected, abs=tol)
+
+
+@pytest.mark.parametrize(
+    "x_min, x_max, x_0", [(None, None, np.ones(2)), (np.array([-1, 0]), np.array([1, 2]), None)]
+)
+def test_newton_fix_point(x_min, x_max, x_0):
+    expected = np.array([0, 0.7390851332151607])
+    tol = 1e-7
+    result = newton_fix_point(
+        jacobian_function=jacfunctionfix(),
+        vectorial_function=functionfix(),
+        x_min=x_min,
+        x_max=x_max,
+        x_0=x_0,
+        tol=tol,
+    )
+    assert result.success
+    assert result.root == pytest.approx(expected, abs=tol)
